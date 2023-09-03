ee134477ff6ed1ba0d947f59749f409cd878480b
priyanshu <priyanshumaurya2810@gmail.com>
2023-09-03 11:50:54 +0530

    parsing the codeforce problem page

--- codeforcesParser.py
+++ codeforcesParser.py
@@ -10,5 +10,10 @@ def main():
     url = url.replace('ID', inf[0])
     url = url.replace('N', inf[1].upper())
 
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        pre_elements = soup.find_all("pre")
+
 if __name__ == "__main__":
     main()
