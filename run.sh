004e3ddcb5b052c23f1192ebf928562c3b3fe655
priyanshu <priyanshumaurya2810@gmail.com>
2023-09-03 12:12:12 +0530

    comparing outputCodeforces.txt and output.txt whether ouput and answer are matching or not

--- run.sh
+++ run.sh
@@ -7,3 +7,13 @@ g++ $1.cpp -o $1
 echo "$contestId $1" | python "/path/codeforceParser.py"
 
 cat inputCodeforces.txt | ./$1 > output.txt
+file1="output.txt"
+file2="outputCodeforces.txt"
+
+if cmp -s "$file1" "$file2";
+then
+    echo -e "All TestCase Passed.\n"
+else
+    echo -e "Wrong Answer.\n"
+    #cmp "$file1" "$file2"
+fi
