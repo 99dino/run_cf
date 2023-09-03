14e56de3299ac2ddda4f19f4b7e66c7f98a43b8d
priyanshu <priyanshumaurya2810@gmail.com>
2023-09-03 12:44:45 +0530

    fixed minor error

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 # this particular script assuming the source code for the problem is written in c++ programming language, you can alter this script according to your preferred programming language ans use the executable command for those programming language where c++ code compiling and executing is done 
 
-# stored the current working directory in variable 'path'
-path=$(pwd)
+# stored the current working directory in variable 'directory'
+directory=$(pwd)
 # extracted bottom most directory from path
 # which will be the ContestId
-contestId="${path##*/}"
+contestId="${directory##*/}"
 
 # runnning the codeforcesParser.py to parse and return the number of set of test cases
 testCase=$(echo "$contestId $1 0" | python "path/codeforceParser.py")
