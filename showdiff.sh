191cc2492019a0149b5cd2e5eb3d9dd1feb0f3db
priyanshu <priyanshumaurya2810@gmail.com>
2023-09-03 12:14:11 +0530

    showdiff.sh script that outputs the outputCodeforces.txt and output.txt file to see the differnces

---
+++ showdiff.sh
@@ -0,0 +1,8 @@
+# your code output's
+echo -e "-----------\nYour Output\n-----------"
+cat output.txt
+
+# actual correct answer on codeforces
+echo -e "--------------\nCorrect Answer\n--------------"
+cat outputCodeforces.txt
+sleep 5
