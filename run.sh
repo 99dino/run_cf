34c944a9035c6b51b8ebdb9f8707222c08723db6
priyanshu <priyanshumaurya2810@gmail.com>
2023-09-03 12:18:31 +0530

    making run.sh script to work automatically for all set of test cases

--- run.sh
+++ run.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/bash
 path=$(pwd)
 contestId="${path##*/}"
-
+testCase=$(echo "$contestId $1 0" | python "/path/codeforceParser.py")
 
 g++ $1.cpp -o $1
-echo "$contestId $1" | python "/path/codeforceParser.py"
+for (( i = 1 ; i <= $testCase ; i++ ))
+do
+    echo "$contestId $1 $i" | python "/path/codeforceParser.py"
 
-cat inputCodeforces.txt | ./$1 > output.txt
-file1="output.txt"
-file2="outputCodeforces.txt"
+    cat inputCodeforces.txt | ./$1 > output.txt
 
-if cmp -s "$file1" "$file2";
-then
-    echo -e "All TestCase Passed.\n"
-else
-    echo -e "Wrong Answer.\n"
-    #cmp "$file1" "$file2"
-fi
+    file1="output.txt"
+    file2="outputCodeforces.txt"
 
+    if cmp -s "$file1" "$file2";
+    then
+        echo -e "All TestCase Passed.\n"
+    else
+        echo -e "Wrong Answer.\n"
+        #cmp "$file1" "$file2"
+    fi
+done
+# to hold the shell for 5 seconds before auto closing
 sleep 5
